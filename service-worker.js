3bec9d6da7a54b9b7e6b58dd1cd40b9930a7cac6
iasjem <jemimah.beryl.sai@gmail.com>
2021-02-17 10:51:42 +0800

    new deployment

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/orbis/precache-manifest.497ea3bf75c35a24e2e78c3f72d235c7.js"
+  "/orbis/precache-manifest.21dfca7d0eb88e8c8db5a91b1323b8ef.js"
 );
 
 self.addEventListener('message', (event) => {
